1b835a35b74abfed9e0d80e4b04ffe81eb6adcd4
Sanjana Farhin Astha <sastha2420212@bscse.uiu.ac.bd>
2026-03-07 05:06:49 +0000

    vector file entry

--- learningCPP/09.Vector/introcduction to vector and initialization/introduction_to_vector_.cpp
+++ learningCPP/09.Vector/introcduction to vector and initialization/introduction_to_vector_.cpp
@@ -27,50 +27,41 @@ void print_vector(vector<int>VEC)
 int main()
 {
    /*
-      NOTE 2.
-
-      How to initalize vector element?
-
-      vector <datatype> vector name; --> initial empty vector
-
-      vector <datatype> vector name={value of elements}; --> initialize element directly in vector
-      Ex: 
-
-      vector <datatype> vector name (vector size, value of element); --> pre -initialize given element in the vector
-
-      vector <datatype> vector name=(other vector name to have the same size other vector);
-
-
-
-      NOTE 3.
+    NOTE 2.
 
       vector elements will be work as for h loop
 
       for(vector index : vector name)
       {
-      // copy yur function
+      // copy your function
       }
 
 
    */
-  //vector <datatype> vector name; --> initial empty vector
-   
+  
+   /*vector <datatype> vector name; --> initial empty vector
+   vector content NULL element
+   */
 
+  vector<int>vec;
+   
+  // vector <datatype> vector name={value of elements}; --> initialize element directly in vector
    vector<int> vec_1 = {10, 20, 30, 40, 50};
    cout << "vector 1 : \n";
    print_vector(vec_1); 
    cout << "\n\n";
 
-   // 2
+   /*vector <datatype> vector name (vector size, value of element); --> pre -initialize given element in the vector
+   that mean suppose vector<int>vector_name(4,10) then the vector will contant {10 10 10 10}
+   */
 
    vector<int> vec_2(4, 100);
    cout << "vector 2 : \n";
    print_vector(vec_2);
 
    cout << "\n\n";
-
-   // 3
-
+  
+   // vector <datatype> vector name=(other vector name to have the same size other vector);
    vector<int> vec_3(vec_1);
    cout << "vector 3 : \n";
    print_vector(vec_3);
